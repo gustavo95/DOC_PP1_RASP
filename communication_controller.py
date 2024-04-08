bdbce64f4ae5e5cf525c87de39445262c375fa65
Gustavo <gustavocosta@ic.ufal.br>
2024-04-08 18:17:29 -0300

    feat: :sparkles: Ajustado para suportar imagens maiores e rgb

--- communication_controller.py
+++ communication_controller.py
@@ -1,14 +1,17 @@
+import cv2
 import time
 import numpy as np
 from rpi5_spi import RPi5SPI
 
 class CommunicationController:
 
-    def __init__(self) -> None:
+    def __init__(self, height, width) -> None:
         self.spi = RPi5SPI()
         self.frequency = 12000000
         self.delay_time = 1/self.frequency
         self.spi.set_frequency(self.frequency)
+        self.height = height
+        self.width = width
 
     def sendbyte(self, byte_to_send):
         # time.sleep(self.delay_time)
@@ -31,13 +34,15 @@ class CommunicationController:
         self.sendbyte(0b10000001)
         self.sendbyte(0b00000000)
 
-    def send_img(self, img) -> None:
+    def send_img(self, img, channel = 0b10) -> None:
 
-        self.sendbyte(np.uint8(1))
+        self.sendbyte(0b00000100 | channel)
 
         height, width = img.shape[0:2]
 
         print(width, height)
+
+        # time.sleep(2)
 
         height_bytes = self.toUnint8(height, 2)
         width_bytes = self.toUnint8(width, 2)
@@ -46,6 +51,8 @@ class CommunicationController:
         self.sendbyte(height_bytes[1])
         self.sendbyte(width_bytes[0])
         self.sendbyte(width_bytes[1])
+
+        # time.sleep(2)
 
         initial_time = time.time()
         for y in range(height):
@@ -59,28 +66,37 @@ class CommunicationController:
         send_time = time.time() - initial_time
         print(f"Time to send image: {send_time}")
 
-    def recive_img(self):
-        self.sendbyte(np.uint8(2))
+    def recive_img(self, channel = 0b10):
+        self.sendbyte(0b00001000 | channel)
 
-        new_img = np.zeros((120, 160), dtype=np.uint8)
+        new_img = np.zeros((self.height, self.width), dtype=np.uint8)
         x = 0
         y = 0
 
         # time.sleep(1)
         pixel = self.sendbyte(0) # discart first byte
 
-        for i in range(19200):
+        for i in range(76800):
             pixel = self.sendbyte(0)
             new_img[y, x] = pixel
             x += 1
-            if(x >= 160):
+            if(x >= self.width):
                 y += 1
                 x = 0
-                if (y >= 120):
+                if (y >= self.height):
                     break
             # time.sleep(1)
-        return new_img
 
+        self.sendbyte(0)
+        return new_img
+    
+    def send_rgb_img(self, img):
+
+        channel_b, channel_g, channel_r = cv2.split(img)
+
+        self.send_img(channel_r, 0b01)
+        self.send_img(channel_g, 0b10)
+        self.send_img(channel_b, 0b11)
 
     def toUnint8(self, data, num_bytes) -> np.uint8:
         data_bytes = data.to_bytes(num_bytes, "big")
--- main.py
+++ main.py
@@ -3,23 +3,38 @@ import time
 from communication_controller import CommunicationController
 
 def main():
-    com = CommunicationController()
-
+    height = 240
+    width = 320
+    com = CommunicationController(height, width)
+    
     img = cv2.imread('hand.jpg')
-    img = cv2.resize(img, (160, 120))
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.resize(img, (width, height))
 
     cv2.imshow("img", img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-    com.send_img(img)
+    com.send_rgb_img(img)
 
     print("Image send")
     time.sleep(2)
 
-    new_img = com.recive_img()
+    new_img_r = com.recive_img(0b01)
+    cv2.imshow("new_img_r", new_img_r)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
+    new_img_g = com.recive_img(0b10)
+    cv2.imshow("new_img_g", new_img_g)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    new_img_b = com.recive_img(0b11)
+    cv2.imshow("new_img_b", new_img_b)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    new_img = cv2.merge([new_img_b, new_img_g, new_img_r])
     cv2.imshow("new_img", new_img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
